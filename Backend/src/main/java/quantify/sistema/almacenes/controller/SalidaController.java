76d070d78cb178316a43ee9d0e8edfc49050e2e8
LoP-1 <miguel.castillo.valero@gmail.com>
2025-10-17 13:39:18 -0500

    fix menor 2

--- Backend/src/main/java/quantify/sistema/almacenes/controller/SalidaController.java
+++ Backend/src/main/java/quantify/sistema/almacenes/controller/SalidaController.java
@@ -1,11 +1,15 @@
 package quantify.sistema.almacenes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import quantify.sistema.almacenes.record.salida.SalidaDTO;
 import quantify.sistema.almacenes.record.salida.SalidaProductosDTO;
 import quantify.sistema.almacenes.service.SalidaService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/salidas")
@@ -20,7 +24,10 @@ public class SalidaController {
         return "ok";
     }
 
-
+    @GetMapping()
+    public List<SalidaDTO> listarSalidas(){
+        return salidaService.listarSalidas();
+    }
 
 
 }
